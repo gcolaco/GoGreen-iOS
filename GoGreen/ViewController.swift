74b12b62b8d193af110f5f210abcd135d77eb900
Gustavo Colaco <gustavocolaco@Gustavos-MacBook-Pro.local>
2018-10-05 17:31:01 -0300

    First commit

---
+++ GoGreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoGreen
+//
+//  Created by Gustavo Colaco on 05/10/18.
+//  Copyright © 2018 Gustavo Colaco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
