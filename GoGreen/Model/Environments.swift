5c60fa184175875ac06bbb16e2d6eb8ebf5e84bd
Gustavo Colaco <gustavocolaco@Gustavos-MacBook-Pro.local>
2018-10-12 00:53:56 -0300

    Add the rest of the views and their relations

---
+++ GoGreen/Model/Environments.swift
@@ -0,0 +1,14 @@
+//
+//  Environments.swift
+//  GoGreen
+//
+//  Created by Gustavo Colaco on 07/10/18.
+//  Copyright © 2018 Gustavo Colaco. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Environments: NSManagedObject {
+
+}
