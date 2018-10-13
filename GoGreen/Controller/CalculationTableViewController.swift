bccb318e9b6a64a43db1f108a86e343d435d6c1b
Gustavo Colaco <gustavocolaco@Gustavos-MacBook-Pro.local>
2018-10-13 15:30:01 -0300

    saving environments itens with core data

--- GoGreen/Controller/CalculationTableViewController.swift
+++ GoGreen/Controller/CalculationTableViewController.swift
@@ -7,84 +7,102 @@
 //
 
 import UIKit
+import CoreData
 
 class CalculationTableViewController: UITableViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var environmentsList: [Environments] = []
+//    var array: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadElement()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
 
+
+    @IBAction func addEnvironment(_ sender: Any) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Informe o nome do ambiente a ser adicionado", message: "", preferredStyle: .alert)
+        let add = UIAlertAction(title: "Adicionar", style: .default) { (action) in
+           self.addElement(textField: textField)
+        }
+        
+        let cancel = UIAlertAction(title: "cancelar", style: .cancel, handler: nil)
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Ex.: Sala"
+            textField = alertTextField
+        }
+        
+        alert.addAction(add)
+        alert.addAction(cancel)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    //add a new environment into the environmentsList and save it in core data database
+    func addElement(textField: UITextField) {
+        let newEnvironment = Environments(context: self.context)
+        newEnvironment.name = textField.text
+        self.environmentsList.append(newEnvironment)
+        
+        do{
+            try self.context.save()
+        }catch{
+            print(error.localizedDescription)
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    //load the elements in environmetsList, and show it in our tableview
+    func loadElement() {
+        let request : NSFetchRequest<Environments> = Environments.fetchRequest()
+        
+        do {
+            environmentsList = try context.fetch(request)
+        }catch{
+            print(error.localizedDescription)
+        }
+        
+        
+    }
+    
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return environmentsList.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "calculationCell", for: indexPath)
+        
+        cell.textLabel?.text = environmentsList[indexPath.row].name
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+ 
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
